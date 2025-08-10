00e85f48001cd5c4022161090a1b906c4c964ed1
Kritikashahi11 <20029784@students.koi.edu.au>
2025-08-10 20:10:07 +1000

    firstprogram

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] myNumbers = { 5, 1, 8, 9 };
+
+Console.WriteLine(myNumbers.Max());          //maximum number
+Console.WriteLine(myNumbers.Min());          //minimun number
+Console.WriteLine(myNumbers.Sum());          //sum of the number
