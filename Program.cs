abb374e5d7fdaf619bbb343d7abcee6d50454df6
Kritikashahi11 <20029784@students.koi.edu.au>
2025-08-10 20:19:14 +1000

    2 changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] myNumbers = { 5, 1, 8, 9 };
+int[] myNumbers = { 5, 1, 8, 9, 4 }; // Added a new number
 
-Console.WriteLine(myNumbers.Max());          //maximum number
-Console.WriteLine(myNumbers.Min());          //minimun number
-Console.WriteLine(myNumbers.Sum());          //sum of the number
+Console.WriteLine(myNumbers.Max());          // maximum number
+Console.WriteLine(myNumbers.Min());          // minimum number
+Console.WriteLine(myNumbers.Average());      // average of the numbers
--- obj/Release/net8.0/Tutorial3Ex2.AssemblyInfo.cs
+++ obj/Release/net8.0/Tutorial3Ex2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial3Ex2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00e85f48001cd5c4022161090a1b906c4c964ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial3Ex2")]
